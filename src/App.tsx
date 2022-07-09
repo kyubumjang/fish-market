55eab9523851f196550fd6fa63a37b48eed52cf1
Kyubum Jang <jkb2221@gmail.com>
2022-07-10 00:53:28 +0900

    feat(App): Add 다크모드 적용 및 글로벌 스타일 적용, 라우팅 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { HomePage, NotFoundPage } from './pages';
 import Layout from './layout/layout';
-import { HomePage } from './pages';
+import GlobalStyle from './styles/GlobalStyle';
+import { darkTheme, lightTheme } from './themes';
+import {
+	createTheme,
+	ThemeProvider,
+	PaletteMode,
+	CssBaseline,
+} from '@mui/material';
+import { ColorContext } from './contexts/ColorContext';
 
 function App() {
+	const [mode, setMode] = useState<PaletteMode>('light');
+	const colorMode = useMemo(
+		() => ({
+			toggleColorMode: () => {
+				setMode((prevMode: PaletteMode) =>
+					prevMode === 'light' ? 'dark' : 'light'
+				);
+			},
+		}),
+		[]
+	);
+	const theme = useMemo(
+		() => createTheme(mode === 'light' ? lightTheme : darkTheme),
+		[mode]
+	);
+
 	return (
-		<div className='App'>
-			<Layout>
-				<Routes>
-					<Route path='/' element={<HomePage />} />
-				</Routes>
-			</Layout>
-		</div>
+		<ColorContext.Provider value={colorMode}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline enableColorScheme />
+				<div className='App'>
+					<GlobalStyle />
+					<Routes>
+						<Route path='/' element={<Layout />}>
+							<Route index element={<HomePage />} />
+						</Route>
+						<Route path='*' element={<NotFoundPage />} />
+					</Routes>
+				</div>
+			</ThemeProvider>
+		</ColorContext.Provider>
 	);
 }
 
