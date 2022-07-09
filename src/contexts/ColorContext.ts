dadcadf0f1ccc535db93637fc6601ddd72bc3381
Kyubum Jang <jkb2221@gmail.com>
2022-07-10 00:48:48 +0900

    feat: Add ColorContext 추가

---
+++ src/contexts/ColorContext.ts
@@ -0,0 +1,8 @@
+import { createContext } from 'react';
+interface ColorContextSchema {
+	toggleColorMode: () => void;
+}
+
+export const ColorContext = createContext<ColorContextSchema>(
+	{} as ColorContextSchema
+);
