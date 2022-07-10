df847e87702c31c8895e8271143439b5c306d9aa
Kyubum Jang <jkb2221@gmail.com>
2022-07-10 20:18:37 +0900

    refactor: Delete React.StrictMode

--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,9 @@ const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.StrictMode>
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
 );
 
 reportWebVitals();
