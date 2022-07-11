3cc3808d9949d9df4ade0cc643a6a4cffa555399
Kyubum Jang <jkb2221@gmail.com>
2022-07-11 18:49:58 +0900

    feat: Add 유투브 라우팅 추가, react-query provider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { HomePage, NotFoundPage } from './pages';
 import Layout from './layout/layout';
+import { HomePage, NotFoundPage, YoutubePage } from './pages';
 import GlobalStyle from './styles/GlobalStyle';
 import { darkTheme, lightTheme } from './themes';
 import {
@@ -11,6 +11,9 @@ import {
 	CssBaseline,
 } from '@mui/material';
 import { ColorContext } from './contexts/ColorContext';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+const queryClient = new QueryClient();
 
 function App() {
 	const [mode, setMode] = useState<PaletteMode>('light');
@@ -28,22 +31,29 @@ function App() {
 		() => createTheme(mode === 'light' ? lightTheme : darkTheme),
 		[mode]
 	);
-
 	return (
-		<ColorContext.Provider value={colorMode}>
-			<ThemeProvider theme={theme}>
-				<CssBaseline enableColorScheme />
-				<div className='App'>
-					<GlobalStyle />
-					<Routes>
-						<Route path='/' element={<Layout />}>
-							<Route index element={<HomePage />} />
-						</Route>
-						<Route path='*' element={<NotFoundPage />} />
-					</Routes>
-				</div>
-			</ThemeProvider>
-		</ColorContext.Provider>
+		<QueryClientProvider client={queryClient}>
+			<ColorContext.Provider value={colorMode}>
+				<ThemeProvider theme={theme}>
+					<CssBaseline enableColorScheme />
+					<div className='App'>
+						<GlobalStyle />
+						<Routes>
+							<Route path='/' element={<Layout />}>
+								<Route index element={<HomePage />} />
+								<Route path='/youtube' element={<YoutubePage />}></Route>
+								{/* <Route path='/notice' element={<NoticePage />}></Route>
+							<Route path='/recommend/market' element={<MarketPage />}></Route>
+							<Route path='/recommend/anchor' element={<AnchorPage />}></Route>
+							<Route path='/about' element={<AboutPage />}></Route> */}
+							</Route>
+							<Route path='*' element={<NotFoundPage />} />
+						</Routes>
+					</div>
+				</ThemeProvider>
+			</ColorContext.Provider>
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
 	);
 }
 
